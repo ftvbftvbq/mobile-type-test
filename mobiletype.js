7b07d9953d7c794480516f4ecabffaac17888805
lidongdong04 <lidongdong04@baidu.com>
2019-01-02 10:43:35 +0800

    t

--- mobiletype.js
+++ mobiletype.js
@@ -1,5 +1,9 @@
 const typeMap= {
-    "MI 8" : "mi8",
-    "MI 5" : "mi5"
+    "MI 8" : "MI8",
+    "MI 5" : "MI5",
+    "MIX 2" : "MIX2",
+    "(PACM00|PACT00|PAAT00|PAAM00)" : "OPPOR15",
+    "(EML-AL00|EML-TL00)": "HUAWEIP20",
+    "vivo X21": "VIVOX21",
 }
 export default typeMap;
